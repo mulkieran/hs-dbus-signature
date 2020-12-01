cd0c78881a8e6d79355a5ab6ade637d015e8039f
mulhern <amulhern@redhat.com>
2020-11-30 19:05:06 -0500

    Switch to GitHub Actions from Travis CI
    Signed-off-by: mulhern <amulhern@redhat.com>

--- tests/test_hypothesis.py
+++ tests/test_hypothesis.py
@@ -7,7 +7,7 @@ Hypothesis-based testing of the signature producing strategy.
 
 # isort: STDLIB
 import unittest
-from os import environ, sys
+from os import sys
 
 # isort: THIRDPARTY
 from hypothesis import HealthCheck, given, settings, strategies
@@ -17,7 +17,7 @@ from hs_dbus_signature import dbus_signatures
 from hs_dbus_signature._signature import _CODES
 
 settings.register_profile("tracing", deadline=None)
-if sys.gettrace() is not None or environ.get("TRAVIS") is not None:
+if sys.gettrace() is not None:
     settings.load_profile("tracing")
 
 
